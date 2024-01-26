34e8cae5584bc3555ee2662cc1b790573ad1bc03
김민지 <ghi512151@gmail.com>
2024-01-26 09:42:37 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -0,0 +1,13 @@
+n = int(input())
+res = n
+
+for _ in range(n):
+    s = input()
+    for i in range(0, len(s)-1):
+        if s[i] == s[i+1]:
+            pass
+        elif s[i] in s[i+1:]:
+            res -= 1
+            break
+
+print(res)
