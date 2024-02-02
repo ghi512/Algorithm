8eaef0c273072696e0f2f7ff3f5d5844a9b96870
김민지 <ghi512151@gmail.com>
2024-02-02 16:17:48 +0900

    [Silver V] Title: 색종이, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -0,0 +1,13 @@
+numOfPapers = int(input()) # 색종이 개수
+bigPaper = [[0] * 100 for _ in range(100)]
+
+for _ in range(numOfPapers):
+    x,y = map(int, input().split())
+    for i in range(x, x+10):
+        for j in range(y, y+10):
+            bigPaper[i][j] = 1
+
+res = 0
+for i in range(100):
+    res += bigPaper[i].count(1)
+print(res)
