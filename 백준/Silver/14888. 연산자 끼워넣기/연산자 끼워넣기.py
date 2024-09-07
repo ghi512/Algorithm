8eff79ab51634fea3b23ef7852d1428d4e1135a5
김민지 <ghi512151@gmail.com>
2024-09-07 18:41:01 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -0,0 +1,38 @@
+n = int(input())
+nums = list(map(int, input().split()))
+ops = list(map(int, input().split())) # 차례대로 [덧셈,뺄셈,곱셈,나눗셈]
+
+min_num = 1000000001 # 10억
+max_num = -1000000001
+
+def dfs(i, calc):
+    global ops, min_num, max_num
+    if i == n: # n개의 수를 모두 사용했을 때
+        max_num = max(max_num, calc)
+        min_num = min(min_num, calc)
+        return
+
+    if ops[0] > 0: # 덧셈
+        ops[0] -= 1
+        dfs(i+1, calc + nums[i])
+        ops[0] += 1
+
+    if ops[1] > 0:  # 뺄셈
+        ops[1] -= 1
+        dfs(i + 1, calc - nums[i])
+        ops[1] += 1
+
+    if ops[2] > 0:  # 곱셈
+        ops[2] -= 1
+        dfs(i + 1, calc * nums[i])
+        ops[2] += 1
+
+    if ops[3] > 0:  # 나눗셈
+        ops[3] -= 1
+        dfs(i + 1, int(calc / nums[i]))
+        ops[3] += 1
+
+
+dfs(1, nums[0])
+print(max_num)
+print(min_num)
