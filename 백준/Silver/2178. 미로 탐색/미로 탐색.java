288907306a1e89dd054ccbf8451c1d211a0e400c
김민지 <ghi512151@gmail.com>
2026-01-22 16:34:13 +0900

    [Silver I] Title: 미로 탐색, Time: 120 ms, Memory: 15360 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -2,26 +2,25 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
-    // 상 -> 우 -> 하 -> 좌
-    static int[] dx = { 0, 1, 0, -1 };
-    static int[] dy = { 1, 0, -1, 0 };
-    static boolean[][] visited;
+    static int[] dx = { 0, 1, 0, -1};
+    static int[] dy = { 1, 0, -1, 0};
     static int[][] graph;
-    static int N, M;
+    static boolean[][] visited;
+    static int N,M;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st;
 
+        st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+
         graph = new int[N][M];
         visited = new boolean[N][M];
 
         for(int i=0; i<N; i++) {
-            st = new StringTokenizer(br.readLine());
-            String line = st.nextToken();
+            String line = br.readLine();
             for(int j=0; j<M; j++) {
                 graph[i][j] = Integer.parseInt(line.substring(j, j+1));
             }
@@ -31,24 +30,27 @@ public class Main {
         System.out.println(graph[N-1][M-1]);
     }
 
-    public static void BFS(int i, int j) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[] {i, j}); // 큐에 현재 노드 삽입
+    static void BFS(int i,int j) {
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[] {i, j});
         visited[i][j] = true;
 
-        while(!queue.isEmpty()) {
-            int now[] = queue.poll();
+        while(!q.isEmpty()) {
+            int now[] = q.poll();
             for(int k=0; k<4; k++) {
                 int x = now[0] + dx[k];
                 int y = now[1] + dy[k];
-                if( x>=0 && y>=0 && x<N && y<M ) { // 좌표 유효성 검사
+
+                if(x>=0 && y>=0 && x<N && y<M) {
                     if(graph[x][y] != 0 && !visited[x][y]) {
                         visited[x][y] = true;
-                        graph[x][y] = graph[now[0]][now[1]] + 1; // 깊이 업데이트
-                        queue.add(new int[] {x,y});
+                        graph[x][y] = graph[now[0]][now[1]] + 1;
+                        q.add(new int[] {x,y});
                     }
                 }
             }
         }
+
     }
+
 }
