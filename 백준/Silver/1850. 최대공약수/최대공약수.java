076e0198bc5e2a81c17775470f03e343726485a4
김민지 <ghi512151@gmail.com>
2026-01-29 20:17:29 +0900

    [Silver I] Title: 최대공약수, Time: 340 ms, Memory: 18328 KB -BaekjoonHub

---
+++ 백준/Silver/1850. 최대공약수/최대공약수.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long result = gcd(a,b);
+
+        while(result > 0) {
+            bw.write("1");
+            result--;
+        }
+
+        bw.flush();
+        bw.close();
+    }
+
+    static long gcd(long a, long b) {
+        if(b==0) {
+            return a;
+        }
+        else {
+            return gcd(b, a%b);
+        }
+    }
+
+}
