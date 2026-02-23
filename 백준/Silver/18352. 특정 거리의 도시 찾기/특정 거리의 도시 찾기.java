ae78b51808e298848f2ca134c29545d816ce431c
김민지 <ghi512151@gmail.com>
2026-02-23 16:47:08 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1052 ms, Memory: 278544 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -2,43 +2,40 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
     static int N, M, K, X;
     static ArrayList<Integer>[] list;
     static int[] visited;
-    static List<Integer> answer;
+    static ArrayList<Integer> answer;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken()); // 도시 개수
-        M = Integer.parseInt(st.nextToken()); // 도로 개수
-        K = Integer.parseInt(st.nextToken()); // 거리 정보
-        X = Integer.parseInt(st.nextToken()); // 출발 도시 번호
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        X = Integer.parseInt(st.nextToken());
 
-        // 그래프 생성
         list = new ArrayList[N+1];
         for(int i=1; i<=N; i++) {
-            list[i] = new ArrayList<Integer>();
+            list[i] = new ArrayList<>();
         }
 
-        // 그래프 정보 저장
-        for(int i=0; i<M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int S = Integer.parseInt(st.nextToken());
-            int E = Integer.parseInt(st.nextToken());
-            list[S].add(E);
-        }
-
-        // 방문배열 초기화
         visited = new int[N+1];
         for(int i=1; i<=N; i++) {
             visited[i] = -1;
         }
 
         answer = new ArrayList<>();
+
+        for(int i=0; i<M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            list[a].add(b);
+        }
+
         BFS(X);
 
         for(int i=1; i<=N; i++) {
@@ -49,24 +46,26 @@ public class Main {
 
         if(answer.isEmpty()) {
             System.out.println("-1");
-        } else {
+        }
+        else {
             Collections.sort(answer);
-            for(int a : answer) {
-                System.out.println(a);
+            for(int i : answer) {
+                System.out.println(i);
             }
         }
+
     }
 
-    private static void BFS(int Node) {
-        Queue<Integer> q = new LinkedList<>();
-        q.add(Node);
-        visited[Node]++;
+    static void BFS(int start) {
+        Queue<Integer> q = new LinkedList<Integer>();
+        q.add(start);
+        visited[start]++;
 
         while(!q.isEmpty()) {
-            int nowNode = q.poll();
-            for(int i : list[nowNode]) {
+            int now = q.poll();
+            for(int i : list[now]) {
                 if(visited[i] == -1) {
-                    visited[i] = visited[nowNode] + 1;
+                    visited[i] = visited[now] + 1;
                     q.add(i);
                 }
             }
