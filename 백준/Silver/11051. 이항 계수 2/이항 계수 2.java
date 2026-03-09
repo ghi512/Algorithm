f6d69e3ec4f550ee23446eb7373d77721c19ba7f
김민지 <ghi512151@gmail.com>
2026-03-09 18:33:27 +0900

    [Silver II] Title: 이항 계수 2, Time: 188 ms, Memory: 21708 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+    static int N, K;
+    static int[][] D;
+    static int MOD = 10007;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        K = sc.nextInt();
+        D = new int[N+1][N+1];
+
+        for(int i=0; i<=N; i++) {
+            D[i][0] = 1;
+            D[i][i] = 1;
+            D[i][1] = i;
+        }
+
+        for(int i=2; i<=N; i++) {
+            for(int j=1; j<i; j++) {
+                D[i][j] = D[i-1][j] + D[i-1][j-1];
+                D[i][j] %= MOD;
+            }
+        }
+
+        System.out.println(D[N][K]);
+    }
+}
