44bbd349d99bd116e714d649bf6a3dc3232e81c5
김민지 <ghi512151@gmail.com>
2026-04-27 12:17:22 +0900

    [level 3] Title: 거스름돈, Time: 38.63 ms, Memory: 55.2 MB -BaekjoonHub

--- 프로그래머스/3/12907. 거스름돈/거스름돈.java
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -2,7 +2,6 @@ class Solution {
     public int solution(int n, int[] money) {
         int MOD = 1_000_000_007;
         
-        // dp[i] = i원을 만드는 방법의 수
         long[] dp = new long[n+1];
         dp[0] = 1;
         
