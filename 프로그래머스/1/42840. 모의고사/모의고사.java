7c7157882bdc942567483f93e98431c73542b3e2
김민지 <ghi512151@gmail.com>
2026-04-30 21:06:25 +0900

    [level 1] Title: 모의고사, Time: 3.19 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,39 +1,31 @@
-import java.util.stream.IntStream;
+import java.util.*;
 
 class Solution {
-    private static final int[][] RULES = {
-        {1, 2, 3, 4, 5},
-        {2, 1, 2, 3, 2, 4, 2, 5},
-        {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
-    };
-    
-    private int getPicked(int person, int problem) {
-        int[] rule = RULES[person];
-        int index = problem % rule.length;
-        return rule[index];
-    }
-    
     public int[] solution(int[] answers) {
-        int[] corrects = new int[3];
-        int max = 0;
         
-        for(int problem = 0; problem<answers.length; problem++) {
-            int answer = answers[problem];
-            
-            for(int person = 0; person < 3; person++) {
-                int picked = getPicked(person, problem);
-                if(answer == picked) {
-                    if(++corrects[person] > max) {
-                        max = corrects[person];
-                    }
-                }
+        int[] p1 = {1, 2, 3, 4, 5};
+        int[] p2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] p3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        
+        int[] score = new int[3];
+        
+        for(int i=0; i<answers.length; i++) {
+            if(answers[i] == p1[i % p1.length]) score[0]++;
+            if(answers[i] == p2[i % p2.length]) score[1]++;
+            if(answers[i] == p3[i % p3.length]) score[2]++;
+        }
+        
+        int max = Math.max(score[0], Math.max(score[1], score[2]));
+        List<Integer> result = new ArrayList<>();
+        
+        for(int i=0; i<3; i++) {
+            if(score[i] == max) {
+                result.add(i+1);
             }
         }
         
-        final int maxCorrects = max;
-        return IntStream.range(0, 3)
-            .filter(i -> corrects[i] == maxCorrects)
-            .map(i -> i+1)
+        return result.stream()
+            .mapToInt(i -> i)
             .toArray();
     }
 }
