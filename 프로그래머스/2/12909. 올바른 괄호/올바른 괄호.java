26974e8ff11612492f869e40b20abc497b2bc353
김민지 <ghi512151@gmail.com>
2026-04-30 19:44:08 +0900

    [level 2] Title: 올바른 괄호, Time: 17.95 ms, Memory: 55 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,32 +1,26 @@
-import java.util.Stack;
+import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        Stack<String> stack = new Stack<>();
+        Stack<Character> stack = new Stack<>();
         
-        // 1. 문자열 s 한 문자씩 돌며 확인
-        for(int i=0; i<s.length(); i++){
-            char ch = s.charAt(i);
-            // 1-1. '('이면 스택에 넣음
-            if (ch == '(') {
-                stack.push("(");
+        for(int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            
+            if(c == '(') {
+                stack.push('(');
+                continue;
             }
-            // 1-2. ')'이면서 스택이 비어있으면 false 반환
-            else if (stack.isEmpty()) {
+            
+            // ')'인 경우
+            if(stack.empty()) {
                 return false;
             }
-            // 1-3. ')'이면서 스택에 원소가 있으면 pop
             else {
                 stack.pop();
             }
         }
         
-        // 2. 스택의 사이즈가 1 이상이면 false, 아니면 true 반환
-        if (stack.size() > 0) {
-            return false;
-        }
-        else {
-            return true;
-        }
+        return stack.empty();        
     }
 }
