fa4d647f928274f4964bc6335a83a8d6a03e7e55
김민지 <ghi512151@gmail.com>
2026-06-07 20:38:25 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 55.20 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -4,18 +4,21 @@ class Solution {
     public String solution(String[] participant, String[] completion) {
         HashMap<String, Integer> map = new HashMap<>();
         
-        for(String p : participant) {
-            map.put(p, map.getOrDefault(p, 0) + 1);
+        for(String s : participant) {
+            map.put(s, map.getOrDefault(s,0) + 1);
         }
         
         for(String c : completion) {
-            map.put(c, map.get(c) - 1);
+            if(map.get(c) > 1) {
+                map.put(c, map.get(c) - 1);
+            }
+            else {
+                map.remove(c);
+            }
         }
         
         for(String key : map.keySet()) {
-            if(map.get(key) > 0) {
-                return key;
-            }
+            return key;
         }
         
         return "";
