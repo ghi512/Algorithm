50378717d50081ef21a2ffcdd59b8edcfa05ab57
김민지 <ghi512151@gmail.com>
2026-06-09 21:45:40 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 84.34 ms, Memory: 202 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -2,35 +2,43 @@ import java.util.*;
 
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
+        Queue<Integer> wait = new LinkedList<>();
+        Queue<int[]> bridge = new LinkedList<>();
         
-        // 아무것도 없는 상태를 0 각각으로 표현
-        Queue<Integer> bridge = new LinkedList<>();
-        
-        int time = 0;
-        int currentWeight = 0;
-        int idx = 0;
-        
-        for(int i=0; i<bridge_length; i++) {
-            bridge.offer(0);
+        for(int w : truck_weights) {
+            wait.offer(w);
         }
         
-        while(idx < truck_weights.length) {
-            time++;
+        int totalWeight = 0; // 다리 위 트럭 무게
+        int times = 0; // 경과 시간
+        
+        while(!wait.isEmpty() || !bridge.isEmpty()) {
+            times++;
             
-            // 다리에서 하나 나감
-            currentWeight -= bridge.poll();
-            
-            // 다음 트럭
-            if(currentWeight + truck_weights[idx] <= weight) {
-                bridge.offer(truck_weights[idx]);
-                currentWeight += truck_weights[idx++];
+            // 현재 다리 위에 있는 모든 트럭들의 경과 시간 처리
+            int size = bridge.size();
+            for(int i=0; i<size; i++) {
+                int[] truck = bridge.poll();
+                
+                // truck[0]은 트럭의 무게, truck[1]은 트럭의 경과 시간
+                if(truck[1] + 1 > bridge_length) {
+                    totalWeight -= truck[0];
+                }
+                else {
+                    bridge.offer(new int[] {truck[0], truck[1] + 1});
+                }
             }
-            else {
-                bridge.offer(0);
+            
+            // 대기 트럭 중 더 넣을 수 있으면 넣기
+            if(!wait.isEmpty() && bridge.size() < bridge_length) {
+                if(wait.peek() + totalWeight <= weight) {
+                    int truck = wait.poll();
+                    bridge.offer(new int[] {truck, 1});
+                    totalWeight += truck;
+                }
             }
         }
         
-        // 마지막 트럭 나가는 시간
-        return time + bridge_length;
+        return times;
     }
 }
