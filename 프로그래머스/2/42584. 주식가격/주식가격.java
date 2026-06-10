be9110035ee6749df4218fae9837bd7df45d7e29
김민지 <ghi512151@gmail.com>
2026-06-10 19:14:56 +0900

    [level 2] Title: 주식가격, Time: 20.24 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,29 +1,22 @@
-import java.util.Stack;
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] prices) {
-        // 1. 가격이 떨어지지 않은 기간 저장할 배열 선언
-        int n = prices.length;
-        int[] answer = new int[n];
-        
-        // 2. 가격 비교할 인덱스 담을 스택 선언
         Stack<Integer> stack = new Stack<>();
-        stack.push(0);
+        int[] answer = new int[prices.length];
         
-        // 3. prices 순환 돌면서 하나씩 확인
-        for(int i=1; i<n; i++){
-            while(!stack.isEmpty() && prices[i] < prices[stack.peek()]){
-                // 4. 가격이 떨어졌으면 이전 가격의 기간 확정
-                int j = stack.pop();
-                answer[j] = i - j;
+        stack.push(0);
+        for(int i=1; i<prices.length; i++) {
+            while(!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+                int before = stack.pop();
+                answer[before] = i - before;
             }
             stack.push(i);
         }
         
-        // 5. 스택에 남아있으면 가격 떨어지지X
-        while(!stack.isEmpty()){
-            int j = stack.pop();
-            answer[j] = (n-1) - j;
+        while(!stack.isEmpty()) {
+            int idx = stack.pop();
+            answer[idx] = prices.length - 1 - idx;
         }
         
         return answer;
