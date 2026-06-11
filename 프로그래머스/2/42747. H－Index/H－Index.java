96401e9b26766460667bfdbbb86d0e45a43dc778
김민지 <ghi512151@gmail.com>
2026-06-11 18:35:47 +0900

    [level 2] Title: H-Index, Time: 2.30 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -2,18 +2,20 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] citations) {
+        Integer[] arr = new Integer[citations.length];
+        for(int i=0; i<citations.length; i++) {
+            arr[i] = citations[i];
+        }
         
-        Arrays.sort(citations);
-        int n = citations.length;
+        Arrays.sort(arr, (a,b) -> b-a); // [6, 5, 3, 1, 0]
         
-        for(int i=0; i<n; i++) {
-            int h = n - i;
-            
-            if(citations[i] >= h) {
-                return h;
+        int answer = 0;
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i] >= i+1) {
+                answer = i+1;
             }
         }
         
-        return 0;
+        return answer;
     }
 }
