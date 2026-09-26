de81ee1243e9f291773d91781418345651750823
김민지 <ghi512151@gmail.com>
2026-09-26 17:42:20 +0900

    [level 2] Title: 타겟 넘버, Time: 5.16 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,31 +1,30 @@
 import java.util.*;
 
 class Solution {
+    static int target;
+    static int index;
     static int answer = 0;
-    static int[] nums;
-    static int targetNum;
+    static int[] numbers;
     
     public int solution(int[] numbers, int target) {
-        nums = numbers;
-        targetNum = target;
+        this.target = target;
+        this.numbers = numbers;
         
-        dfs(0,0);
+        dfs(0, 0);
+        
         return answer;
     }
     
-    public void dfs(int idx, int sum) {
-        // 모든 숫자를 다 사용한 경우
-        if(idx == nums.length) {
-            if(sum == targetNum) {
+    public void dfs(int index, int sum) {
+        if(index == numbers.length) {
+            if(sum == target) {
                 answer++;
+                return;
             }
             return;
         }
         
-        // 현재 숫자를 더하는 경우
-        dfs(idx+1, sum + nums[idx]);
-        
-        // 현재 숫자를 빼는 경우
-        dfs(idx+1, sum - nums[idx]);
+        dfs(index+1, sum + numbers[index]);
+        dfs(index+1, sum - numbers[index]);
     }
 }
