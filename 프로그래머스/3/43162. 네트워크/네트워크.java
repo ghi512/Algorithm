b1a9fc0f7aed9a8fa28187051e59404782ebbc05
김민지 <ghi512151@gmail.com>
2026-09-26 16:43:46 +0900

    [level 3] Title: 네트워크, Time: 0.60 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -6,37 +6,35 @@ class Solution {
     static int answer = 0;
     
     public int solution(int n, int[][] computers) {
-        graph = new ArrayList[n];
-        for(int i=0; i<n; i++) {
+        graph = new ArrayList[n+1];
+        visited = new boolean[n+1];
+        
+        for(int i=1; i<=n; i++) {
             graph[i] = new ArrayList<>();
         }
-        
-        visited = new boolean[n];
         
         for(int i=0; i<n; i++) {
             for(int j=0; j<n; j++) {
                 if(i == j) continue;
                 if(computers[i][j] == 1) {
-                    graph[i].add(j);
-                    graph[j].add(i);
+                    graph[i+1].add(j+1);
+                    graph[j+1].add(i+1);
                 }
             }
         }
         
-        for(int i=0; i<n; i++) {
+        for(int i=1; i<=n; i++) {
             if(!visited[i]) {
                 dfs(i);
                 answer++;
             }
         }
-        
         return answer;
     }
     
-    public void dfs(int n) {
-        visited[n] = true;
-        
-        for(int next : graph[n]) {
+    public void dfs(int node) {
+        visited[node] = true;
+        for(int next : graph[node]) {
             if(!visited[next]) {
                 dfs(next);
             }
